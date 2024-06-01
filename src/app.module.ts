51fc0afbd0144820db5b668a0e52836c25f6b17e
Stahsf <50431040@qq.com>
2024-06-01 11:55:57 +0800

    feat: 引入MongoDB、Redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,41 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
 
 @Module({
-  imports: [],
+  imports: [
+    // config
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'production' ? '.env.prod' : '.env.local',
+    }),
+    // MongoDB
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: `mongodb://${configService.get<string>('MONGODB_URL')}/`,
+        user: configService.get<string>('MONGODB_USER'),
+        pass: configService.get<string>('MONGODB_PASS'),
+        dbName: configService.get<string>('MONGODB_NAME'),
+      }),
+    }),
+    // Redis
+    RedisModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        config: {
+          host: configService.get('REDIS_HOST'),
+          port: parseInt(configService.get('REDIS_PORT'), 10),
+          password: configService.get('REDIS_PASSWORD'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
