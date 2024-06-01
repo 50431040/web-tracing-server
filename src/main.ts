037ba08cee3c97f1c8e087f726bf210b2071311d
Stahsf <50431040@qq.com>
2024-06-01 12:02:25 +0800

    feat: 参数校验

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  BadRequestException,
+  HttpStatus,
+  ValidationPipe,
+} from '@nestjs/common';
 
 async function bootstrap() {
+  const isProduction = process.env.NODE_ENV === 'prod';
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: (errors) => {
+        const msg = isProduction
+          ? 'Invalid params!'
+          : Object.values(errors[0].constraints)[0];
+        return new BadRequestException({
+          message: msg,
+          code: HttpStatus.BAD_REQUEST,
+        });
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
